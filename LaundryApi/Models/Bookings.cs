d2250c2769ca6d08d4e295101cedd3b364c91119
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-22 20:36:49 +0100

    added Bookings class with properties

--- LaundryApi/Models/Bookings.cs
+++ LaundryApi/Models/Bookings.cs
@@ -5,6 +5,19 @@ namespace LaundryBookings.Models
 {
     public class Booking
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
+        [BsonElement("UserId")]
+        public string UserId { get; set; } = null!;
+        [BsonElement("MachineId")]
+        public string MachineId { get; set; } = null!;
+
+        [BsonElement("StartTime")]
+        public DateTime StartTime { get; set; }
+
+        [BsonElement("EndTime")]
+        public DateTime EndTime { get; set; }
     }
 }
