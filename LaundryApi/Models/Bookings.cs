26241874b644053eaade673d25fa10285cadcd80
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-22 21:31:51 +0100

    changed to _id to match it with mongodb _id when performing crud

--- LaundryApi/Models/Bookings.cs
+++ LaundryApi/Models/Bookings.cs
@@ -7,7 +7,7 @@ namespace LaundryBooking.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+        public string? _id { get; set; }
 
         [BsonElement("UserId")]
         public string UserId { get; set; } = null!;
