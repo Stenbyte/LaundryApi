149bae5855b51da0593303de22194a6677f32233
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-08 21:56:55 +0100

    added jwt token service

---
+++ LaundryApi/Services/JwtService.cs
@@ -0,0 +1,42 @@
+using LaundryApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+public class JwtService
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateJwtToken(SignUpUser user)
+    {
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var secretKey = Encoding.UTF8.GetBytes(jwtSettings["Secret"]!);
+
+        var claims = new List<Claim> {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id!),
+            new Claim(JwtRegisteredClaimNames.Email, user.email),
+            new Claim(ClaimTypes.Role , "User"),
+            new Claim("streetName", user.adress.streetName)
+        };
+
+        var key = new SymmetricSecurityKey(secretKey);
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var expires = DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["AccessTokenExpirationMinutes"]));
+
+        var token = new JwtSecurityToken(
+            issuer: jwtSettings["Issuer"],
+            audience: jwtSettings["Audience"],
+            claims: claims,
+            expires: expires,
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
