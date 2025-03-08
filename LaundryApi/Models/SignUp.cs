c86e58a4b3fe66ada61add5240e9fae22391c4a6
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-08 21:56:42 +0100

    changed name from SignUpUser to User

--- LaundryApi/Models/SignUp.cs
+++ LaundryApi/Models/SignUp.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace LaundryApi.Models
 {
-    public class SignUpUser : IEntity
+    public class User : IEntity
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
