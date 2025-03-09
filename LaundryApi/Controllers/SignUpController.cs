02d81fe7af570da3d900fe04ff7d215aa1cceb82
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-09 20:59:53 +0100

    check for existing user with same email, and using dbName search

--- LaundryApi/Controllers/SignUpController.cs
+++ LaundryApi/Controllers/SignUpController.cs
@@ -5,18 +5,17 @@ using Microsoft.Extensions.Options;
 using LaundryApi.Exceptions;
 using MongoDB.Bson;
 using LaundryApi.Validators;
-using BCrypt.Net;
+
 
 namespace LaundryApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class SignUpController(LaundryService laundryService, IOptions<MongoDBSettings> mongoSettings, SignUpValidator signupValidator, JwtService jwtService) : ControllerBase
+    public class SignUpController(LaundryService laundryService, IOptions<MongoDBSettings> mongoSettings, SignUpValidator signupValidator) : ControllerBase
     {
         private readonly LaundryService _laundryService = laundryService;
         private readonly IOptions<MongoDBSettings> _mongoSettings = mongoSettings;
         private readonly SignUpValidator _validator = signupValidator;
-        private readonly JwtService _jwtService = jwtService;
 
         [HttpPost]
         public async Task<IActionResult> CreateUser(User newUser)
@@ -32,16 +31,18 @@ namespace LaundryApi.Controllers
                 throw new CustomException("Validation", validationResult.Errors, 400);
             }
 
-            var isUserExist = await _laundryService.FindUser(newUser);
+            var userExists = await _laundryService.FindUserByEmail<User>(newUser.email);
+            if (userExists != null)
+            {
+                throw new CustomException("User already exists", "", 403);
+            }
 
-            AddDbNameToUser(newUser, isUserExist);
+            await AddDbNameToUser(newUser);
             var hashPassword = BCrypt.Net.BCrypt.HashPassword(newUser.password);
             newUser.password = hashPassword;
 
             try
             {
-                // check user if added dbName
-
                 await _laundryService.CreateUser(_mongoSettings.Value.UsersCollectionName, newUser);
             }
             catch (CustomException ex)
@@ -51,8 +52,9 @@ namespace LaundryApi.Controllers
             return CreatedAtAction(nameof(CreateUser), new { id = newUser.Id });
         }
 
-        private static User AddDbNameToUser(User newUser, User? existingUser)
+        private async Task<User> AddDbNameToUser(User newUser)
         {
+            var existingUser = await _laundryService.FindExistingUserWithDbName(newUser);
             if (existingUser != null)
             {
                 newUser.dbName = existingUser.dbName;
