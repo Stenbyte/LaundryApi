6e69ce561d389999538d276df7e6f3571a11decb
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-09 09:29:51 +0100

    hashing password upon signup

--- LaundryApi/Controllers/SignUpController.cs
+++ LaundryApi/Controllers/SignUpController.cs
@@ -5,19 +5,21 @@ using Microsoft.Extensions.Options;
 using LaundryApi.Exceptions;
 using MongoDB.Bson;
 using LaundryApi.Validators;
+using BCrypt.Net;
 
 namespace LaundryApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class SignUpController(LaundryService laundryService, IOptions<MongoDBSettings> mongoSettings, SignUpValidator signupValidator) : ControllerBase
+    public class SignUpController(LaundryService laundryService, IOptions<MongoDBSettings> mongoSettings, SignUpValidator signupValidator, JwtService jwtService) : ControllerBase
     {
         private readonly LaundryService _laundryService = laundryService;
         private readonly IOptions<MongoDBSettings> _mongoSettings = mongoSettings;
         private readonly SignUpValidator _validator = signupValidator;
+        private readonly JwtService _jwtService = jwtService;
 
-        [HttpPost]
-        public async Task<IActionResult> Post(User newUser)
+        [HttpPost("signup")]
+        public async Task<IActionResult> CreateUser(User newUser)
         {
             if (string.IsNullOrEmpty(newUser.Id))
             {
@@ -32,16 +34,21 @@ namespace LaundryApi.Controllers
 
             var isUserExist = await _laundryService.FindUser(newUser);
 
+            AddDbNameToUser(newUser, isUserExist);
+            var hashPassword = BCrypt.Net.BCrypt.HashPassword(newUser.password);
+            newUser.password = hashPassword;
+
             try
             {
-                AddDbNameToUser(newUser, isUserExist);
+                // check user if added dbName
+
                 await _laundryService.CreateUser(_mongoSettings.Value.UsersCollectionName, newUser);
             }
             catch (CustomException ex)
             {
                 throw new CustomException("Unable to create new user", ex, 400);
             }
-            return CreatedAtAction(nameof(Post), new { id = newUser.Id });
+            return CreatedAtAction(nameof(CreateUser), new { id = newUser.Id });
         }
 
         private static User AddDbNameToUser(User newUser, User? existingUser)
