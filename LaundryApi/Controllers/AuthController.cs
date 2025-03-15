50f4a06a7f998be893f00fb0949a506d0cf14928
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-15 15:55:13 +0100

    using refreshTokenExpiry from appsettings

--- LaundryApi/Controllers/AuthController.cs
+++ LaundryApi/Controllers/AuthController.cs
@@ -19,17 +19,21 @@ namespace LaundryApi.Controllers
         private readonly JwtService _jwtService;
         private readonly LaundryService _layndryService;
         private readonly LoginValidator _validator;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(JwtService jwtService, LaundryService laundryService, LoginValidator validator)
+
+        public AuthController(JwtService jwtService, LaundryService laundryService, LoginValidator validator, IConfiguration configuration)
         {
             _jwtService = jwtService;
             _layndryService = laundryService;
             _validator = validator;
+            _configuration = configuration;
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
             var validationResult = await _validator.ValidateAsync(request);
             if (!validationResult.IsValid)
             {
@@ -43,7 +47,8 @@ namespace LaundryApi.Controllers
             var token = _jwtService.GenerateJwtToken(user);
             var refreshToken = _jwtService.GenerateRefreshToken();
             user.refreshToken = refreshToken;
-            user.refreshTokenExpiry = DateTime.UtcNow.AddDays(7);
+            user.refreshTokenExpiry = DateTime.UtcNow
+            .AddDays(double.Parse(jwtSettings["RefreshTokenExpirationDays"]!));
             await _layndryService.UpdateUser(user);
             return Ok(new { token, refreshToken });
         }
@@ -60,6 +65,7 @@ namespace LaundryApi.Controllers
         [HttpPost("refresh-token")]
         public async Task<IActionResult> RefreshToken([FromBody] TokenRequest request)
         {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
             User? user = await _layndryService.FindUserByRefreshToken<User>(request.refreshToken);
 
             if (user == null || user.refreshTokenExpiry < DateTime.UtcNow)
@@ -71,8 +77,9 @@ namespace LaundryApi.Controllers
             var newRefreshToken = _jwtService.GenerateRefreshToken();
 
             user.refreshToken = newRefreshToken;
-            // use time from app settings
-            user.refreshTokenExpiry = DateTime.UtcNow.AddDays(7);
+            user.refreshTokenExpiry = DateTime.UtcNow
+            .AddDays(double.Parse(jwtSettings["RefreshTokenExpirationDays"]!));
+
             await _layndryService.UpdateUser(user);
 
             return Ok(new { newAccessToken, newRefreshToken });
