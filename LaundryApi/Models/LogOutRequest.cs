fec30e70435c62272bb6ebdd01e1738d3157eac4
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-22 13:17:32 +0100

    logout request model added

---
+++ LaundryApi/Models/LogOutRequest.cs
@@ -0,0 +1,10 @@
+namespace LaundryApi.Models
+{
+    public class LogOutRequest
+    {
+        /// <summary>
+        /// The user's email address which acts as a user name.
+        /// </summary>
+        public required string email { get; init; }
+    }
+}
