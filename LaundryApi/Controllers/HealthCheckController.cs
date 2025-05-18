f115744ea64ae63fecddddeaa14f48a68462d12a
Sten Scheifel <sten.scheifel@gmail.com>
2025-05-18 16:05:02 +0200

    added controller for check isAlive endpoint, and bit cleanup

---
+++ LaundryApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,44 @@
+using System.Text.Json;
+using LaundryApi.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaundryApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IsAliveController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        private static readonly string InfoFile = "build-info.json";
+
+        public IsAliveController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult isAlive()
+        {
+            var path = Path.Combine(_env.ContentRootPath, InfoFile);
+
+            if (!System.IO.File.Exists(path))
+            {
+                throw new CustomException("Build info file is not found", null, 400);
+            }
+
+            var json = System.IO.File.ReadAllText(path);
+
+            var data = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+
+            return Ok(
+                new {
+                    status = "Alive",
+                    commit = data?["commit"],
+                    commitDate = data?["commitDate"],
+                    buildTime = data?["buildTime"],
+                }
+            );
+        }
+    }
+}
