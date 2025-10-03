bc420c604d01d0597809cd9d34e77181dbdaf95d
Sten Scheifel <sten.scheifel@gmail.com>
2025-10-03 22:01:17 +0200

    dbContext config

--- LaundryApi/DbContext/LaundryDbContext.cs
+++ LaundryApi/DbContext/LaundryDbContext.cs
@@ -1,9 +1,21 @@
 using LaundryApi.Models;
+using LaundryApi.PostgresModels;
 using Microsoft.EntityFrameworkCore;
 
 public class LaundryDbContext : DbContext
 {
     public LaundryDbContext(DbContextOptions<LaundryDbContext> options) : base(options) { }
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<UserPg> Users { get; set; }
+    public DbSet<Property> Property { get; set; }
+    public DbSet<UserProperty> UserProperties { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserProperty>().HasKey(up => new { up.UserId, up.PropertyId });
+
+        modelBuilder.Entity<UserProperty>().HasOne(up => up.User).WithMany(u => u.UserProperties).HasForeignKey(up => up.UserId).OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserProperty>().HasOne(up => up.Property).WithMany(p => p.UserProperties).HasForeignKey(up => up.PropertyId).OnDelete(DeleteBehavior.Cascade);
+    }
 }
