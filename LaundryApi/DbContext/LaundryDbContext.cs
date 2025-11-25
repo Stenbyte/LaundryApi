d60cee190feb8e42ed4cad6bc4f92da7049dee55
Sten Scheifel <sten.scheifel@gmail.com>
2025-11-25 08:50:33 +0100

    refactor for many-to-many relationship

--- LaundryApi/DbContext/LaundryDbContext.cs
+++ LaundryApi/DbContext/LaundryDbContext.cs
@@ -1,6 +1,6 @@
-using LaundryApi.Models;
 using LaundryApi.PostgresModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 public class LaundryDbContext : DbContext
 {
@@ -12,10 +12,15 @@ public class LaundryDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<UserProperty>().HasKey(up => new { up.UserId, up.PropertyId });
 
-        modelBuilder.Entity<UserProperty>().HasOne(up => up.User).WithMany(u => u.UserProperties).HasForeignKey(up => up.UserId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<UserProperty>(e => {
+            e.HasKey(up => new { up.UserId, up.PropertyId });
+            e.HasOne(up => up.User).WithMany(u => u.UserProperties).HasForeignKey(up => up.UserId).OnDelete(DeleteBehavior.Cascade);
+            e.HasOne(up => up.Property).WithMany(p => p.UserProperties).HasForeignKey(up => up.PropertyId).OnDelete(DeleteBehavior.Cascade);
+        });
 
-        modelBuilder.Entity<UserProperty>().HasOne(up => up.Property).WithMany(p => p.UserProperties).HasForeignKey(up => up.PropertyId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<UserPg>(e => {
+            e.HasIndex(u => u.Email).IsUnique();
+        });
     }
 }
