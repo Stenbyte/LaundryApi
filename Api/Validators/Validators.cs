f42191060a40875e043d31daa3576f512943d221
Sten Scheifel <sten.scheifel@gmail.com>
2026-03-30 22:01:48 +0200

    validator updated with new user entity

--- Api/Validators/Validators.cs
+++ Api/Validators/Validators.cs
@@ -1,35 +1,33 @@
 using FluentValidation;
 using TenantApi.Exceptions;
 using TenantApi.Models;
-using Microsoft.AspNetCore.Identity.Data;
 
 namespace TenantApi.Validators
 {
-    public class SignUpValidator : AbstractValidator<User>
+    public class SignUpValidator : AbstractValidator<CreateUserRequest>
     {
         public SignUpValidator()
         {
-            RuleFor(x => x.firstName).NotEmpty()
+            RuleFor(x => x.FirstName).NotEmpty()
             .WithMessage("First Name is required")
             .MaximumLength(50)
             .WithMessage("First name cannot exceed 50 characters");
 
-            RuleFor(x => x.lastName).NotEmpty()
+            RuleFor(x => x.LastName).NotEmpty()
             .WithMessage("Last Name is required")
             .MaximumLength(50)
             .WithMessage("Last name cannot exceed 50 characters");
 
-            RuleFor(x => x.password)
+            RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Password is required")
             .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
             .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
             .Matches(@"\d").WithMessage("Password must contain at least one number")
             .Matches(@"[@$!%*?&]").WithMessage("Password must contain at least one special character");
 
-            RuleFor(x => x.email).NotEmpty().WithMessage("email is required").Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").WithMessage("Invalid email format");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("email is required").Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").WithMessage("Invalid email format");
 
             // RuleFor(x => x.phoneNumber).SetValidator(new PhoneNumberValidator());
-            RuleFor(x => x.adress).SetValidator(new AdressValidator());
         }
     }
 
