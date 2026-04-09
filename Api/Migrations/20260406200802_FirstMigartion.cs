d8ca832361ab6b82ece2abac9e7bc65c5cc06e15
Sten Scheifel <sten.scheifel@gmail.com>
2026-04-09 21:12:10 +0200

    first migration

---
+++ Api/Migrations/20260406200802_FirstMigartion.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TenantApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigartion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "buildings",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    street_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    building_number = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_buildings", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "character varying(24)", maxLength: 24, nullable: false),
+                    is_admin = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "properties",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    unit_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    building_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_properties", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_properties_buildings_building_id",
+                        column: x => x.building_id,
+                        principalTable: "buildings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_properties",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    property_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_properties", x => new { x.user_id, x.property_id });
+                    table.ForeignKey(
+                        name: "FK_user_properties_properties_property_id",
+                        column: x => x.property_id,
+                        principalTable: "properties",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_properties_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_properties_building_id",
+                table: "properties",
+                column: "building_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_properties_property_id",
+                table: "user_properties",
+                column: "property_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_properties");
+
+            migrationBuilder.DropTable(
+                name: "properties");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "buildings");
+        }
+    }
+}
